ac845151727950cb040619195c10c36f820b1785
Daniel Vianna <dvianna@ausnetservices.com.au>
2019-03-19 15:20:59 +1100

    add first test

---
+++ tests/PhotoGrooveTests.elm
@@ -0,0 +1,17 @@
+module PhotoGrooveTests exposing (decoderTest)
+
+import Expect exposing (Expectation)
+import Json.Decode exposing (decodeString)
+import PhotoGroove
+import Test exposing (..)
+
+
+decoderTest : Test
+decoderTest =
+    test "title defaults to (untitled)" <|
+        \_ ->
+            """{"url": "fruits.com", "size": 5}"""
+                |> decodeString PhotoGroove.photoDecoder
+                |> Result.map .title
+                |> Expect.equal
+                    (Ok "(untitled)")
